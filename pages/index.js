98d942fa16832857ecf92fa84753dd30ff4efa11
rohangajbhe89 <97128922+rohangajbhe89@users.noreply.github.com>
2022-01-04 16:26:18 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my world!" />
+        <Header title="Rohan's World!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
